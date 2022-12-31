6e7e2413a89b04932d75ec590af4341d5384f6da
siyabuddeen <66248397+Mahammed-Siyabuddeen@users.noreply.github.com>
2022-12-31 16:28:30 +0530

    backend-api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API=axios.create({
-    baseURL:'https://whatsapp-clone-shihab.herokuapp.com'
+    baseURL:'https://whatsapp-g0af.onrender.com/'
 })
 API.interceptors.request.use((req)=>{
     if(localStorage.getItem('chatProfile')){
